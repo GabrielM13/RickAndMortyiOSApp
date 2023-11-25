36d6aa58f924ca63aa9df211540257f503bbc0fe
GabrielM13 <gmarinhosilva@outlook.com>
2023-11-25 04:01:56 -0300

    RM-001 - API Service

--- RickAndMortyiOSApp/RickAndMorty-APP/Network/RMRequest.swift
+++ RickAndMortyiOSApp/RickAndMorty-APP/Network/RMRequest.swift
@@ -8,5 +8,54 @@
 import Foundation
 
 final class RMRequest {
+    private struct Constants {
+        static let baseUrl = "https://rickandmortyapi.com/api"
+    }
     
+    private let endpoint: RMEndpoint
+    
+    private let pathComponents: [String]
+    
+    private let queryParametes: [URLQueryItem]
+    
+    private var urlString: String {
+        var string = Constants.baseUrl
+        string += "/"
+        string += endpoint.rawValue
+        
+        if !pathComponents.isEmpty {
+            pathComponents.forEach({
+                string += "/\($0)"
+            })
+        }
+        
+        if !queryParametes.isEmpty {
+            string += "?"
+            let argumentString = queryParametes.compactMap({
+                guard let value = $0.value else { return nil }
+                return "\($0.name)=\(value)"
+            }).joined(separator: "&")
+            string += argumentString
+        }
+        return string
+    }
+    
+    public var url: URL? {
+        return URL(string: urlString)
+    }
+    
+    public let httpMethod = "GET"
+    
+    public init(endpoint: RMEndpoint,
+                pathComponents: [String] = [],
+                queryParametes: [URLQueryItem] = []
+    ) {
+        self.endpoint = endpoint
+        self.pathComponents = pathComponents
+        self.queryParametes = queryParametes
+    }
+}
+
+extension RMRequest {
+    static let listCharactersRequests = RMRequest(endpoint: .character)
 }
--- RickAndMortyiOSApp/RickAndMorty-APP/Network/RMService.swift
+++ RickAndMortyiOSApp/RickAndMorty-APP/Network/RMService.swift
@@ -9,11 +9,49 @@ import Foundation
 
 ///  API Service para pegar os dados do Rick and Morty
 final class RMService {
+    
     static let shared = RMService()
     
     private init() {}
     
-    public func execute(_ request: RMRequest, completion: @escaping () -> Void) {
+    enum RMServiceError: Error {
+        case failedCreateRequest
+        case failedToGetData
+    }
+    
+    public func execute<T: Codable>(
+        _ request: RMRequest,
+        expecting type: T.Type,
+        completion: @escaping (Result<T, Error>) -> Void
+    ) {
+        guard let urlRequest = self.request(from: request) else {
+            completion(.failure(RMServiceError.failedCreateRequest))
+            return
+        }
         
+        let task = URLSession.shared.dataTask(with: urlRequest) { data, _, error in
+            guard let data = data, error == nil else {
+                completion(.failure(error ?? RMServiceError.failedToGetData))
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(type.self, from: data)
+                completion(.success(result))
+            }
+            catch {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+    
+    // MARK: - API CALL
+    
+    private func request(from rmRequest: RMRequest) -> URLRequest? {
+        guard let url = rmRequest.url else { return nil }
+        var request = URLRequest(url: url)
+        request.httpMethod = rmRequest.httpMethod
+        return request
     }
 }
